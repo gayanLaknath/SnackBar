a84b46d63323eba50b3837aa08bc3a5fda1c253c
gayanLaknath <38901554+gayanLaknath@users.noreply.github.com>
2020-12-23 18:56:42 +0530

    create custom snackbar layout

--- SnackBar/app/src/main/java/com/example/snackbar/MainActivity.java
+++ SnackBar/app/src/main/java/com/example/snackbar/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout mainLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainLayout = findViewById(R.id.main);
     }
 
     public void defaultClick(View view) {
@@ -59,5 +65,28 @@ public class MainActivity extends AppCompatActivity {
         textView.setMaxLines(5);
         textView.setTextColor(Color.YELLOW);
         snackbar.show();
+    }
+
+    public void clickButton(View view) {
+
+        Snackbar snackbar = Snackbar.make(mainLayout,"",Snackbar.LENGTH_LONG);
+        View custom = getLayoutInflater().inflate(R.layout.snackbar_custom,null);
+
+        snackbar.getView().setBackgroundColor(Color.TRANSPARENT);
+        Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
+
+        snackbarLayout.setPadding(0,0,0,0);
+
+        (custom.findViewById(R.id.actionText)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Click the text", Toast.LENGTH_SHORT).show();
+                snackbar.dismiss();
+            }
+        });
+
+        snackbarLayout.addView(custom,0);
+        snackbar.show();
+
     }
 }
